8d2b01632b3bc0f841d7daf849d25a4336c10e98
Kevin Sartori <kevin.bouty@hotmail.fr>
2020-02-27 13:52:40 +0100

    First commit on branch

--- IKEAListenerr/Database.cs
+++ IKEAListenerr/Database.cs
@@ -8,6 +8,21 @@ namespace IKEAListenerr
 {
     class Database
     {
+        public static string GetConnectionString()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Projet2_BancAppli"].ConnectionString;
+            return connectionString;
+        }
+        public static void NonQuerySQL(string queryString)
+        {
+            SqlConnection connection = new SqlConnection(GetConnectionString());
+            connection.Open();
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine("Command executed");
+        }
+
         public IEnumerable<Transaction> GetPendingTransactions(DateTime startDate, DateTime endDate)
         {
 
